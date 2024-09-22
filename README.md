f4792ed7cb48606074cc3106e1d8b74485cb8386
mp <patel.mihir2024@gmail.com>
2024-09-22 10:38:17 -0400

    Fixed engine flameout before explosion

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# Vehicles-Explode-When-Flipped
-A ScriptHookVDotNet3 mod for GTA V which adds roll damage and engine explosions to vehicles similar to the 3D era games.
+# Vehicles Explode When Flipped
+
+- [Description and Features](#description-and-features)
+- [Requirements](#requirements)
+- [How to Install](#how-to-install)
+
+## Description and Features
+
+All vehicles can now take damage when they are flipped. Once they take enough damage they will catch on fire and explode similar to the 3D games.
+
+- All vehicles will now take additional damage when rolling on ground.
+- All vehicles will now explode once the engine catches fire instead of flaming out.
+
+## Requirements
+
+1. Download and install [ScriptHookV](https://www.dev-c.com/gtav/scripthookv)
+2. Download and install ScriptHookVDotNet depending on your game version.
+- **Game version v1.0.3258.0 or later**: [ScriptHookVDotNet-v3.6.0-nightly.119](https://github.com/scripthookvdotnet/scripthookvdotnet-nightly/releases/tag/v3.6.0-nightly.119)
+- **Game version v1.0.3179.0 or earlier**: [ScriptHookVDotNet-v3.6.0](https://github.com/scripthookvdotnet/scripthookvdotnet)
+
+## How to Install
+
+1. Download and extract the "VehiclesExplodeWhenFlipped.zip" archive.
+2. Copy "VehiclesExplodeWhenFlipped.dll" into the "scripts" folder of your GTA V directory.
---
+++ Vehicles Explode When Flipped/Main.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GTA;
+using GTA.Math;
+using GTA.UI;
+
+namespace Vehicles_Explode_When_Flipped {
+    public class Main : Script {
+
+        public Main() {
+            Tick += OnTick;
+            KeyDown += OnKeyDown;
+            KeyUp += OnKeyUp;
+        }
+
+        public void OnTick(object sender, EventArgs eventArgs) {
+
+            //Game.Player.WantedLevel = 0;
+            //Game.Player.Character.IsInvincible = true;
+
+            //TextElement Status = new TextElement(
+            //    $"Engine: {Game.Player.Character.LastVehicle.EngineHealth} ~n~" +
+            //    $"{Game.Player.Character.LastVehicle.IsEngineRunning} ~n~" +
+            //    $"Tank: {Game.Player.Character.LastVehicle.PetrolTankHealth}",
+            //    new PointF(50f, 360f),     // Text position on a 1280x720 pixel space.
+            //    0.4f,                      // Text scaling factor. 
+            //    Color.GhostWhite,          // Text color
+            //    GTA.UI.Font.ChaletLondon,  // Font 
+            //    Alignment.Left,            // Text alignment
+            //    true,                      // Text shadow
+            //    true                       // Text outline
+            //);
+            //Status.Draw();
+
+            foreach (Vehicle vehicle in World.GetAllVehicles()) {
+                if (vehicle.EngineHealth > -1f && !vehicle.IsInAir && vehicle.Type != VehicleType.Bicycle && (vehicle.Rotation.Y > 90f || vehicle.Rotation.Y < -90f)) {
+                    vehicle.EngineHealth -= 1;
+                }
+                else if (vehicle.EngineHealth < -3650) {
+                    vehicle.Explode();
+                }
+            }
+            
+        }
+        public void OnKeyDown(object sender, KeyEventArgs e) { }
+        public void OnKeyUp(object sender, KeyEventArgs e) { }
+    }
+}
